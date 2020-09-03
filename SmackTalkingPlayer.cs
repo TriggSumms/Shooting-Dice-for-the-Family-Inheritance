e91c17d45938b8a427b5a20bce402e0a7ede3629
Trigg Summs <stsumms@gmail.com>
2020-09-02 19:13:35 -0500

    Maybe on the right path

--- SmackTalkingPlayer.cs
+++ SmackTalkingPlayer.cs
@@ -3,8 +3,36 @@ namespace ShootingDice
     // TODO: Complete this class
 
     // A Player who shouts a taunt every time they roll dice
-    public class SmackTalkingPlayer
+    public class SmackTalkingPlayer : Player
     {
-        public string Taunt { get; }
+        public string Taunt { get; set; }
+
+        public override void Play(Player other)
+        {
+            "Youve never been good at square marbles",
+            "Come to think about it your bad at the sperical-shaped marbles too",
+            "Your soooooooooo stupid",
+            "You got rocks in ya head Dangus"
+        };
+
+        int myRoll = Roll();
+        int otherRoll = other.Roll();
+
+        Console.WriteLine($"{Name} rolls a {myRoll}");
+        Console.WriteLine($"{other.Name} rolls a {otherRoll}");
+        if (myRoll > otherRoll)
+        {
+            Console.WriteLine($"{Name} Wins!");
+        }
+        else if (myRoll < otherRoll)
+        {
+            Console.WriteLine($"{other.Name} Wins!");
+        }
+        else
+        {
+            // if the rolls are equal it's a tie
+            Console.WriteLine("It's a tie");
+        }
     }
+
 }
